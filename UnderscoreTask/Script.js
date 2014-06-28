4d0194537c7c514d35c1ce8cd1458b4928d1ec22
Dmitry Osipov <dmitryosipow@gmail.com>
2014-06-28 17:48:38 +0300

    Main site info and articles info are loading,parsing and templating one by one

--- UnderscoreTask/Script.js
+++ UnderscoreTask/Script.js
@@ -1,70 +1,87 @@
 var rememberTop = 0;
 
 window.onscroll = (function(e) {
-    parallaxScroll();
+  parallaxScroll();
 });
 
 function parallaxScroll() {
-    var scrollTop = (window.pageYOffset !== undefined) ?
-        window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    var el = document.querySelector(".navigationMenu");
-    var top = el.offsetTop;
-    if(top && top < scrollTop ){
-        rememberTop = top;
-        el.classList.add('fixedMenu');
-    }
-    if(rememberTop && scrollTop < rememberTop){
-        el.classList.remove('fixedMenu');
-    }
+  var scrollTop = (window.pageYOffset !== undefined) ?
+    window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  var el = document.querySelector(".navigationMenu");
+  var top = el.offsetTop;
+  if(top && top < scrollTop ){
+    rememberTop = top;
+    el.classList.add('fixedMenu');
+  }
+  if(rememberTop && scrollTop < rememberTop){
+    el.classList.remove('fixedMenu');
+  }
 }
 
+//Promise for loading json
+function getRequest(url) {
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = function() {
+      if (request.status == 200) {
+        resolve(request.response); // we get the data here, so resolve the Promise
+      } else {
+        reject(Error(request.statusText)); // if status is not 200 OK, reject.
+      }
+    };
+    request.onerror = function() {
+      reject(Error("Error getting data.")); // error occurred, so reject the Promise
+    };
+    request.send(); // send the request
+  });
+}
 
+//load main site info and then articles
+getRequest('mainInfo.json').then(function(response) {
+  //parsing and templating main site info
+  var data = JSON.parse(response);
+  var compiled = _.template(
+    "<li>" +
+      "<a href='#'><%= title %></a>" +
+      "</li>"
+  );
+  var toAppendString = "";
+  for (var i = 0; i < data.navigationMenu.length; i++) {
+    data.navigationMenu[i].title = data.navigationMenu[i].title.toUpperCase();
+    toAppendString += compiled(data.navigationMenu[i]);
+  }
+  var div = document.querySelector(".navigationMenu").getElementsByTagName("ul")[0];
+  div.innerHTML = toAppendString;
+  div.childNodes[0].classList.add('selected');
 
-var xhr = new XMLHttpRequest();
-xhr.onload = function(){
-    //var compiled = _.template(this.response);
-    var data = JSON.parse(this.response);
-    var tpl = document.getElementById('articleTmp').innerText;
+  var tpl = document.getElementById('sideArticleTmp').textContent;
+  compiled = _.template(tpl);
+  toAppendString = "";
+  for (var i = 0; i < data.sideInfo.length; i++) {
+    toAppendString += compiled(data.sideInfo[i]);
+  }
+  var div = document.getElementsByTagName("aside")[0];
+  div.innerHTML = toAppendString + div.innerHTML;
+  return getRequest('posts.json');
+}).then(function(response) {
+    //after main site info parsing all articles
+    var data = JSON.parse(response);
+    var tpl = document.getElementById('articleTmp').textContent;
     var compiled = _.template(tpl);
     var toAppendString = "";
     for (var i = 0; i < data.length; i++) {
-        toAppendString += compiled(data[i]);
+      toAppendString += compiled(data[i]);
     }
     var div = document.querySelector(".articles");
     div.innerHTML = toAppendString;
-    //document.getElementsByTagName('body')[0].appendChild(div);
-
-};
-xhr.open('GET', 'posts.json');
-xhr.send();
-
-
-
-
-/*
-<!--article>
-    <div>
-        <img src="images/img1.jpg">
-        </div>
-        <h3>
-            <a href="#">Blogotitle of blogopost about blogoflowers</a>
-        </h3>
-        <div class="articleInfo">
-            <time>22:58 Jan 01, 2014</time>
-            <div class="author">by E. Hyperraccoon</div>
-            <div class="social">
-                <a href="#" class="fb">16</a>
-                <a href="#" class="goo">7</a>
-                <a href="#" class="twi">15</a>
-                <a href="#" class="vk">16</a>
-                <a href="#" class="ya">1</a>
-            </div>
-        </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consequatur dolore dolores dolorum
-        expedita facere ipsa iure laborum minima mollitia nobis, officiis, quas qui quis quos tempora,
-        veritatis voluptate voluptatibus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque deleniti
-        dolores iusto maxime porro quisquam quos voluptates! A ab ad architecto atque deleniti, ea eligendi eum facilis
-        illo incidunt inventore iste iusto labore laudantium libero mollitia nisi omnis quibusdam quidem quos reiciendis
-        rem repellat repudiandae rerum, sit suscipit tenetur voluptate.
-        </p>
-    </article-->*/
+    var divTag = document.querySelector(".tags");
+    var compiledTag = _.template(
+      "<a href='#'><%= obj %></a>"
+    );
+    toAppendString = "";
+    for (var i = 0; i < data[0].tags.length; i++) {
+      toAppendString += compiledTag(data[0].tags[i]);
+    }
+    divTag.innerHTML = toAppendString;
+  });
