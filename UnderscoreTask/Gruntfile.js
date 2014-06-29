d00134a5e4dfc5c4ee95ac2b41464b1f8c3746ba
Dmitry Osipov <dmitryosipow@gmail.com>
2014-06-30 01:49:56 +0300

    Added minified by grunt file

---
+++ UnderscoreTask/Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // 1. Вся настройка находится здесь
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      build: {
+        src: 'Script.js',
+        dest: 'Script.min.js'
+      }
+    }
+
+  });
+
+  // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+  grunt.registerTask('default', ['uglify']);
+
+};
